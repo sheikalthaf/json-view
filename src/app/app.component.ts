9c27c7566f0e34f96d7ff71b9afffec1148b04b5
Sheik Althaf <althafmech@outlook.com>
2024-05-24 00:35:52 +0530

    upgrade: update to v18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { JsonComponent } from './json.component';
   standalone: true,
   imports: [RouterOutlet, JsonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'json-view';
